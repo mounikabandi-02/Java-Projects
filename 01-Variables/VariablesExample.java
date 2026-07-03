9de3404d354a21a712e8190740d8e1604d9529bc
Mounika Bandi <mohanmounika263@gmail.com>
2026-07-02 20:27:40 -0400

    Add VariablesExample program

---
+++ 01-Variables/VariablesExample.java
@@ -0,0 +1,17 @@
+public class VariablesExample {
+
+    public static void main(String[] args) {
+
+        String name = "Mounika";
+        int age = 24;
+        double cgpa = 3.8;
+        char grade = 'A';
+        boolean isStudent = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + isStudent);
+    }
+}
