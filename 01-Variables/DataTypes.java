54928a9d2c5c58eb2a42a5d89c804fc8531d2d61
Mounika Bandi <mohanmounika263@gmail.com>
2026-07-02 20:33:23 -0400

    Add DataTypes program

---
+++ 01-Variables/DataTypes.java
@@ -0,0 +1,25 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        byte age = 24;
+        short year = 2026;
+        int salary = 70000;
+        long population = 8000000000L;
+
+        float percentage = 95.5f;
+        double cgpa = 3.85;
+
+        char grade = 'A';
+        boolean passed = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Year: " + year);
+        System.out.println("Salary: " + salary);
+        System.out.println("Population: " + population);
+        System.out.println("Percentage: " + percentage);
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Grade: " + grade);
+        System.out.println("Passed: " + passed);
+    }
+}
