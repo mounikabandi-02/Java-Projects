7084f607d6f862e27fd3fab58658643af43d04b3
Mounika Bandi <mohanmounika263@gmail.com>
2026-07-02 20:16:24 -0400

    Add HelloWorld Java program

---
+++ 01-Variables/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("My name is Mounika.");
+        System.out.println("Welcome to my Java GitHub repository.");
+    }
+
+}
